8d45403fecdd1a231a09d7b0ebc38ce10ead4107
Azhar Arrozak <azhardrozak2001@upi.edu>
2024-10-30 14:27:39 +0700

    first commit

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'mapel' => 'INFORMATIKA',
+    'kelas' => '7 dan 8',
+    'tugas' => [
+        'Perangkat Keras',
+        'Perangkat Lunak',
+        'Jaringan Komputer'
+    ],
+    'file' => ['docx', 'pdf', 'zip'],
+    'ukuran' => 100000000,
+    'kirim_ulang' => false
+];
+
